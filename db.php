0aa3c7e964453f918dc258be578190bf12ce6a40
rahmat setiawan <49220808+rahmatsetiawangk@users.noreply.github.com>
2025-10-21 13:32:21 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
-$host = "7fsr2l.h.filess.io";
-$user = "db1_rahmatsetiawan_armcardpot";
-$pass = "p94b5bed426a509fc0cbf5c5fb7c2bdbd35e3100b";
-$dbname = "db1_rahmatsetiawan_armcardpot";
+$servername = "7fsr2l.h.filess.io";
+$username = "db1_rahmatsetiawan_armcardpot"; // ganti jika perlu
+$password = "p94b5bed426a509fc0cbf5c5fb7c2bdbd35e3100b"; // ganti jika punya password
+$database = "db1_rahmatsetiawan_armcardpot"; // ganti dengan nama database kamu
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
